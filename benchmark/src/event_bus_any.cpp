bb41501ae364657be1da09f11b5d9240b75c0e11
Paul Tsouchlos <developer.paul.123@gmail.com>
2024-09-16 21:41:08 -0600

    feat: add a very basic benchmark setup

---
+++ benchmark/src/event_bus_any.cpp
@@ -0,0 +1,44 @@
+#include <doctest/doctest.h>
+#include <nanobench.h>
+
+#include <chrono>
+#include <eventbus/event_bus.hpp>
+#include <string>
+#include <vector>
+
+TEST_CASE("event dispatch with eventbus using std::any") {
+    std::vector<std::size_t> args = {1'000, 10'000, 100'000, 1'000'000};
+
+    using namespace std::chrono_literals;
+    for (const auto& dispatch_count : args) {
+        ankerl::nanobench::Bench bench;
+        auto bench_title =
+            std::string("event dispatch - " + std::to_string(dispatch_count) + " times");
+        bench.title(bench_title).warmup(100).minEpochIterations(100);
+        bench.timeUnit(1us, "us");
+
+        bench.run([dispatch_count]() {
+            dp::event_bus bus{};
+            struct event {
+                int data_int;
+                float data_float;
+                double data_double;
+                std::uint64_t data_int_large;
+            };
+
+            struct event2 {
+                int data_int;
+            };
+
+            auto registration1 = bus.register_handler<event>([] {});
+            auto registration2 = bus.register_handler<event2>([] {});
+            ankerl::nanobench::doNotOptimizeAway(registration1);
+            ankerl::nanobench::doNotOptimizeAway(registration2);
+
+            for (std::size_t i = 0; i < dispatch_count; ++i) {
+                bus.fire_event(event{});
+                bus.fire_event(event2{});
+            }
+        });
+    }
+}
