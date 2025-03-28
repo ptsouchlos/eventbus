2cb34d4cca0e34df54bf868c320d05ee5731965a
Paul Tsouchlos <developer.paul.123@gmail.com>
2025-03-28 13:33:47 -0400

    feat: add benchmark for any v. variant
    Also added new project options as well to better control what gets build.

---
+++ benchmark/src/event_bus.cpp
@@ -0,0 +1,60 @@
+#include <doctest/doctest.h>
+#include <nanobench.h>
+
+#include <chrono>
+#include <eventbus/event_bus.hpp>
+#include <string>
+#include <vector>
+
+TEST_CASE("event dispatch - std::any vs std::variant") {
+    // This test compares the performance of event dispatching using std::any and std::variant
+    std::vector<std::size_t> args = {1'000, 10'000, 100'000, 1'000'000};
+
+    using namespace std::chrono_literals;
+    for (const auto& dispatch_count : args) {
+        ankerl::nanobench::Bench bench;
+        auto bench_title =
+            std::string("event dispatch - " + std::to_string(dispatch_count) + " times");
+        bench.title(bench_title).relative(true).warmup(100).minEpochIterations(1000);
+        bench.timeUnit(1us, "us");
+
+        struct event {
+            int data_int;
+            float data_float;
+            double data_double;
+            std::uint64_t data_int_large;
+        };
+
+        struct event2 {
+            int data_int;
+        };
+
+        bench.run("std::any", [dispatch_count]() {
+            dp::event_bus bus{};
+
+            auto registration1 = bus.register_handler<event>([] {});
+            auto registration2 = bus.register_handler<event2>([] {});
+            ankerl::nanobench::doNotOptimizeAway(registration1);
+            ankerl::nanobench::doNotOptimizeAway(registration2);
+
+            for (std::size_t i = 0; i < dispatch_count; ++i) {
+                bus.fire_event(event{});
+                bus.fire_event(event2{});
+            }
+        });
+
+        bench.run("std::variant", [dispatch_count]() {
+            auto bus = dp::make_event_bus_for_types<event, event2>();
+
+            auto registration1 = bus.register_handler<event>([] {});
+            auto registration2 = bus.register_handler<event2>([] {});
+            ankerl::nanobench::doNotOptimizeAway(registration1);
+            ankerl::nanobench::doNotOptimizeAway(registration2);
+
+            for (std::size_t i = 0; i < dispatch_count; ++i) {
+                bus.fire_event(event{});
+                bus.fire_event(event2{});
+            }
+        });
+    }
+}
